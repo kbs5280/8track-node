b4c82ed6cbad152527a41dcfe1a52400a5478aec
Kris Sparks <kbs5280@gmail.com>
2017-02-24 17:00:41 -0700

    Create tables in database
    Create playlists
    Create songs
    Create playlist songs
    Run latest migration
    Ready to create seed file

---
+++ db/migrations/20170224112337_initial.js
@@ -0,0 +1,39 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('playlists', function(table) {
+      table.increments('id').primary();
+      table.text('name');
+
+      table.timestamps();
+    }),
+
+    knex.schema.createTable('songs', function(table) {
+      table.increments('id').primary();
+      table.text('title');
+      table.text('artist');
+
+      table.timestamps();
+    }),
+
+    knex.schema.createTable('playlist_songs', function(table) {
+      table.increments('id').primary();
+      table.integer('playlist_id')
+           .references('id')
+           .inTable('playlists');
+      table.integer('song_id')
+           .references('id')
+           .inTable('songs');
+
+      table.timestamps();
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('playlists'),
+    knex.schema.dropTable('songs'),
+    knex.schema.dropTable('playlist_songs')
+  ])
+};
