60b61089432fb77cb3785717d27701b292f33f89
Kris Sparks <kbs5280@gmail.com>
2017-03-07 16:44:43 -0700

    Timestamp bug fixed
    created_at and updated_at populating correctly
    Seed file updated for correct configuration
    Need to update seed file with real data
    Database not currently seeded
    Ready to add tests

--- db/seeds/dev/songs.js
+++ db/seeds/dev/songs.js
@@ -3,58 +3,40 @@ exports.seed = function(knex, Promise) {
   .then(() => {
     return Promise.all([
       knex('songs').insert({
-        id: "xxxxy",
         title: 'Title 1',
         artist_id: 1,
-        created_at: new Date
       }),
       knex('songs').insert({
-        id: "xxxyx",
         title: 'Title 2',
         artist_id: 1,
-        created_at: new Date
       }),
       knex('songs').insert({
-        id: "xxyxx",
         title: 'Title 3',
         artist_id: 1,
-        created_at: new Date
       }),
       knex('songs').insert({
-        id: "xyxxx",
         title: 'Title 4',
         artist_id: 2,
-        created_at: new Date
       }),
       knex('songs').insert({
-        id: "yxxxx",
         title: 'Title 5',
         artist_id: 2,
-        created_at: new Date
       }),
       knex('songs').insert({
-        id: "xxxyy",
         title: 'Title 6',
         artist_id: 2,
-        created_at: new Date
       }),
       knex('songs').insert({
-        id: "xxyyx",
         title: 'Title 7',
         artist_id: 3,
-        created_at: new Date
       }),
       knex('songs').insert({
-        id: ,"xyyxx"
         title: 'Title 8',
         artist_id: 3,
-        created_at: new Date
       }),
       knex('songs').insert({
-        id: "yxxyy",
         title: 'Title 9',
         artist_id: 3,
-        created_at: new Date
       })
     ]);
   });
